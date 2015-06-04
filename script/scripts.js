eabc89cca12a2b893abdebd24b268dd4b085c1ae
daniel <daniel.hernandez.vicente@gmail.com>
2015-06-04 17:31:34 +0200

    script added, tittle added

--- script/scripts.js
+++ script/scripts.js
@@ -3,3 +3,10 @@ $("#button-writting").click( function()
       location.href = 'writing.html';
    }
 );
+
+$("#button-notes").click( function()
+   {
+      location.href = 'notesBook.html';
+   }
+);
+
