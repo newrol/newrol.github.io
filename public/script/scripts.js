5d96c9af418baab1cfc3ce1e9a92cbbe8d71fd33
daniel <daniel.hernandez.vicente@gmail.com>
2015-07-21 00:33:07 +0200

    material design libraries have been added, header has been changed

---
+++ public/script/scripts.js
@@ -0,0 +1,12 @@
+$("#button-writting").click( function()
+   {
+      location.href = '/writing.html';
+   }
+);
+
+$("#button-notes").click( function()
+   {
+      location.href = '/notesBook.html';
+   }
+);
+
